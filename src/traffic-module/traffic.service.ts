6828bea94f92bac94e8fec05067a32cb054664c8
pr0m3th3usEx <pr0m3th3us@d3us.ex>
2020-12-03 23:02:36 +0100

    Update: TrafficService class Singleton

--- src/traffic-module/traffic.service.ts
+++ src/traffic-module/traffic.service.ts
@@ -1,3 +1,24 @@
+import { User } from 'twitter-d';
+import Twitter from '../twitter-sdk/twitter';
+
 export default class TrafficService {
-	// constructor() {}
+	private T: Twitter;
+
+	private static instance: TrafficService | undefined = undefined;
+
+	private constructor() {
+		this.T = new Twitter(process.env.TW_BEARER_TOKEN);
+	}
+
+	async getUser(username: string): Promise<void> {
+		const user = await this.T.GetUserByUsername("PoCInnovation");
+		console.log(user);
+	}
+
+	static getInstance(): TrafficService {
+		if (!this.instance) {
+			this.instance = new TrafficService();
+		}
+		return this.instance;
+	}
 }
