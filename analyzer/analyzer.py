4c69a46179b32eae481edfaee1492b4eb3fc4f83
Pr0m3th3usEx <thomas1.michel@epitech.eu>
2020-12-12 17:25:48 +0100

    feat: Get begin and/or end dates if available in sentence

--- analyzer/analyzer.py
+++ analyzer/analyzer.py
@@ -1,32 +1,18 @@
 import json
 from sys import stdin
 import nltk
-from nltk.tokenize import word_tokenize, TweetTokenizer, sent_tokenize
-from nltk.corpus import stopwords, wordnet
-from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
 import emoji
 import re
-
-lemmatizer = WordNetLemmatizer()
-
+from cltk.tokenizers.line import LineTokenizer
+import datefinder
+from datetime import datetime
 
 stop_words = set(stopwords.words("french"))
 stop_words.remove('au')
 
 punct = ['.', ',' ':', '...', '?', ';', '!']
-
-
-def to_wordnet_tag(tag):
-  if tag.startswith('J'):
-    return wordnet.ADJ
-  elif tag.startswith('N'):
-    return wordnet.NOUN
-  elif tag.startswith('V'):
-    return wordnet.VERB
-  elif tag.startswith('R'):
-    return wordnet
-  else:
-    return None
 
 ## Récupération du corpus de tweets grâce via stdin
 def getJsonData():
@@ -34,39 +20,41 @@ def getJsonData():
     for line in stdin:
         lines.append(line)
     return json.loads("".join(lines))
-  
 
 def process_sentence(sentence):
     tknzr = TweetTokenizer()
-    tagged = nltk.pos_tag([ w for w in tknzr.tokenize(sentence) if w not in punct if w != b"\xef\xb8\x8f".decode('utf-8')  if w not in stop_words])
+    tagged = [ w for w in tknzr.tokenize(sentence) if w not in punct if w != b"\xef\xb8\x8f".decode('utf-8')  if w not in stop_words]
+    print(tagged)
     
-    wordnet_tagged = map(lambda x: (x[0], to_wordnet_tag(x[1])), tagged)
-    lemmatized_sent = []
-    for word, tag in wordnet_tagged:
-      if tag is None:
-        lemmatized_sent.append(word)
-      else:
-        lemmatized_sent.append(lemmatizer.lemmatize(word, tag))
-    print(lemmatized_sent)
 
-
-def create_sentence(data):
-    for i in range(len(data)):
-      data[i]["text"] = ". ".join(re.sub(r'\\n+', '\n', data[i]["text"]).split("\n"))
-# Run semantic analysis and deliver informations with functional format
 def semantic_analysis(data):
   pass
 
-if __name__ == "__main__":
-    data = getJsonData()
-    create_sentence(data)
-    tweet = data[8]
-    sentences = [ s for s in sent_tokenize(tweet["text"]) if s not in punct ]
-    process_sentence(sentences[3])
-    # words = [ w for w in tknzr.tokenize(tweet["text"]) if w != b"\xef\xb8\x8f".decode('utf-8') if w not in stop_words ]
 
-   
-    # print(words)
-    # result = list(map(semantic_analysis, data))
-    # print(result)
+def getTimeDisruption(sentence):
+  begin_date = None
+  end_date = None
+  matches = datefinder.find_dates(sentence)
+  valid_dates = [ d for d in matches if d.year >= datetime.today().year]
+  if len(valid_dates) >= 2:
+    begin_date = valid_dates[0]
+    end_date = valid_dates[1]
+  elif len(valid_dates) == 1:
+    begin_date = valid_dates[0]
+  else:
+    return None
+  return begin_date, end_date
+
+
+if __name__ == "__main__":
+    # p = parsedatetime.Calendar()
+    data = getJsonData()
+    # create_sentence(data)
+    for tweet in data:
+      print(tweet["text"])
+    # tweet = data[8]
     
+      sent_tknzr = LineTokenizer("french")
+      sentences = [ s.lower() for s in sent_tknzr.tokenize(tweet["text"]) if s not in punct ]
+      for sentence in sentences:
+        print(getTimeDisruption(sentence))
