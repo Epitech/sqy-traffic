ff0a38cb8f348bda69a6a8a53a337315b05f9990
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-02-01 17:42:26 +0100

    [feat] refresh wasProcessed field after process

--- src/gtfs-module/disruptions.service.ts
+++ src/gtfs-module/disruptions.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common"
 import { Disruption } from "@prisma/client"
 import { PrismaService } from "src/prisma.service"
+import * as GtfsData from "./gtfs.data"
 
 @Injectable()
 export default class DisruptionService {
@@ -8,27 +9,27 @@ export default class DisruptionService {
 
   async storeNewDisruptions(): Promise<void> {
     console.log("Storing new disruptions found!")
-    // await this.prisma.disruption.create({
-    //   data: {
-    //     cause: GtfsData.AlertCause.ACCIDENT,
-    //     effect: GtfsData.AlertCause.CONSTRUCTION,
-    //     start_date: new Date(Date.now()),
-    //     end_date: new Date(Date.now() + 10 * 60 * 1000),
-    //     description: "Some description",
-    //     routeId: "AO123",
-    //     severity: GtfsData.AlertSeverity.UNKNOWN_SEVERITY,
-    //     wasProcessed: false,
-    //     tweet: {
-    //       connect: {
-    //         tweetId: "ID",
-    //       },
-    //     },
-    //   },
-    // })
+    await this.prisma.disruption.create({
+      data: {
+        cause: GtfsData.AlertCause.ACCIDENT,
+        effect: GtfsData.AlertCause.CONSTRUCTION,
+        start_date: new Date(Date.now()),
+        end_date: new Date(Date.now() + 10 * 60 * 1000),
+        description: "Some description",
+        routeId: "AO123",
+        severity: GtfsData.AlertSeverity.UNKNOWN_SEVERITY,
+        wasProcessed: false,
+        tweet: {
+          connect: {
+            tweetId: "NO_ID",
+          },
+        },
+      },
+    })
   }
 
   async getUnprocessedDisruptions(): Promise<Disruption[]> {
-    return this.prisma.disruption.findMany({
+    const unprocessedDisruptions = await this.prisma.disruption.findMany({
       where: {
         wasProcessed: false,
       },
@@ -36,5 +37,18 @@ export default class DisruptionService {
         tweet: true,
       },
     })
+    await Promise.all(
+      unprocessedDisruptions.map((disruption) =>
+        this.prisma.disruption.update({
+          where: {
+            id: disruption.id,
+          },
+          data: {
+            wasProcessed: true,
+          },
+        }),
+      ),
+    )
+    return unprocessedDisruptions
   }
 }
