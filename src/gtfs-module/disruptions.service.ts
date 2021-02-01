8dbd6281bec6d37f8d59d02a9b85a01deaf91e5e
pr0m3th3usEx <thomas1.michel@epitech.eu>
2021-02-01 17:50:58 +0100

    Remove endpoint tests disruptionsk storage

--- src/gtfs-module/disruptions.service.ts
+++ src/gtfs-module/disruptions.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@nestjs/common"
 import { Disruption } from "@prisma/client"
 import { PrismaService } from "src/prisma.service"
-import * as GtfsData from "./gtfs.data"
+// import * as GtfsData from "./gtfs.data"
 
 @Injectable()
 export default class DisruptionService {
@@ -9,23 +9,23 @@ export default class DisruptionService {
 
   async storeNewDisruptions(): Promise<void> {
     console.log("Storing new disruptions found!")
-    await this.prisma.disruption.create({
-      data: {
-        cause: GtfsData.AlertCause.ACCIDENT,
-        effect: GtfsData.AlertCause.CONSTRUCTION,
-        start_date: new Date(Date.now()),
-        end_date: new Date(Date.now() + 10 * 60 * 1000),
-        description: "Some description",
-        routeId: "AO123",
-        severity: GtfsData.AlertSeverity.UNKNOWN_SEVERITY,
-        wasProcessed: false,
-        tweet: {
-          connect: {
-            tweetId: "NO_ID",
-          },
-        },
-      },
-    })
+    // await this.prisma.disruption.create({
+    //   data: {
+    //     cause: GtfsData.AlertCause.ACCIDENT,
+    //     effect: GtfsData.AlertCause.CONSTRUCTION,
+    //     start_date: new Date(Date.now()),
+    //     end_date: new Date(Date.now() + 10 * 60 * 1000),
+    //     description: "Some description",
+    //     routeId: "AO123",
+    //     severity: GtfsData.AlertSeverity.UNKNOWN_SEVERITY,
+    //     wasProcessed: false,
+    //     tweet: {
+    //       connect: {
+    //         tweetId: "NO_ID",
+    //       },
+    //     },
+    //   },
+    // })
   }
 
   async getUnprocessedDisruptions(): Promise<Disruption[]> {
--- src/gtfs-module/gtfs.module.ts
+++ src/gtfs-module/gtfs.module.ts
@@ -1,12 +1,11 @@
 import { Module } from "@nestjs/common"
 import { PrismaService } from "src/prisma.service"
 import DisruptionService from "./disruptions.service"
-import GtfsController from "./gtfs.controller"
 import { GtfsService } from "./gtfs.service"
 
 @Module({
   imports: [],
   providers: [PrismaService, GtfsService, DisruptionService],
-  controllers: [GtfsController],
+  controllers: [],
 })
 export default class GtfsModule {}
