c5ac39d29f5be269e06e6dbf73d603e2d41469ff
JeromeCGithub <jerome.collet@epitech.eu>
2021-03-01 20:10:55 +0100

    fix: tweet fetch problem

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import { NestFactory } from "@nestjs/core"
-import { PrismaClient } from "@prisma/client"
-import { exec } from "child_process"
 import AppModule from "./app.module"
 import { PORT } from "../config/environnement"
 
@@ -10,24 +8,4 @@ async function main() {
   app.listen(PORT)
 }
 
-async function initDb() {
-  const client = new PrismaClient()
-  await client.$connect().then(() => {
-    exec("npm run migrate", (err, stdout, stderr) => {
-      if (err) {
-        console.info(stderr)
-        process.exit(1)
-      }
-      console.info(stdout)
-    })
-    exec("npm run seed", (err, stdout, stderr) => {
-      if (err) {
-        console.info(stderr)
-      }
-      console.info(stdout)
-    })
-  })
-}
-
-initDb()
 main()
