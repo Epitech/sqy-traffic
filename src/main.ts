0e48f9bcea8a7dae7ab7b7e15780c83d4ca8a706
JeromeCGithub <jerome.collet@epitech.eu>
2021-03-01 19:20:16 +0100

    add: db initialisation on api launch

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core"
 import { PORT } from "../config/environnement"
 import AppModule from "./app.module"
+import { PrismaClient } from "@prisma/client"
+import { exec } from "child_process"
 
 async function main() {
   const app = await NestFactory.create(AppModule)
@@ -8,4 +10,24 @@ async function main() {
   app.listen(PORT)
 }
 
+async function initDb() {
+  const client = new PrismaClient();
+  await client.$connect().then(() => {
+    exec('npm run migrate', (err, stdout, stderr) => {
+      if (err) {
+        console.info(stderr)
+        process.exit(1)
+      }
+      console.info(stdout)
+    })
+    exec('npm run seed', (err, stdout, stderr) => {
+      if (err) {
+        console.info(stderr)
+      }
+      console.info(stdout)
+    })
+  })
+}
+
+initDb()
 main()
--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,19 +1,10 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from "@nestjs/common"
 import { PrismaClient } from "@prisma/client"
-import { exec } from "child_process"
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
-    await this.$connect().then(() => {
-      exec('yes "" | npm run migrate', (err, stdout, stderr) => {
-        if (err) {
-          console.info(stderr)
-          process.exit(1)
-        }
-        console.info(stdout)
-      })
-    })
+    await this.$connect();
   }
 
   async onModuleDestroy() {
